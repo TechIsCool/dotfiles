f56e339843e73aacfaf4b09844fa42bbc6e73598
David Beck <techiscool@gmail.com>
2017-09-29 15:53:50 -0700

    [Refactor] PS1 Console Size

--- powershell/WindowsPowerShell/components/console.ps1
+++ powershell/WindowsPowerShell/components/console.ps1
@@ -1,10 +1,12 @@
-$console = $host.UI.RawUI
-$buffer = $console.BufferSize
-$buffer.Width = $console.MaxWindowSize.Width
-$buffer.Height = 5000
+if (Test-Path variable:global:psISE) { 
+  Write-Debug 'Skipping ISE since there are no WindowSize Settings'
+  return; 
+}
 
-$console.BufferSize = $buffer
-$size = $console.WindowSize
-$size.Width = $console.MaxWindowSize.Width - 10
-$size.Height = 50
-$console.WindowSize = $size
+$max = $host.UI.RawUI.MaxPhysicalWindowSize
+
+$height = $($max.Height / 2)
+if ($height -le 50){ $height = 50 }
+
+$host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.Size($($max.Width-5),9999)
+$host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.Size($($max.Width-6),$height)
